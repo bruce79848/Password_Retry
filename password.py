a5a320423ea42c87d356d705e0ece74441272c3c
Bruce <bruce79848@gmail.com>
2020-03-11 17:31:08 +0800

    Upload an improved version

--- password.py
+++ password.py
@@ -1,12 +1,12 @@
 x = 3
 while x > 0:
+	x = x - 1
 	password = input('Please input the password: ')
 	if password == 'a123456':
 		print('Log in successfully!')
 		break
 	else:
-		print('You can still try', x - 1 , 'times.')
-		x = x - 1
-		if x <= 0:
+		if x > 0:
+			print('You can still try', x , 'times.')
+		else:
 			print('Please contact your bank.')
-			break
